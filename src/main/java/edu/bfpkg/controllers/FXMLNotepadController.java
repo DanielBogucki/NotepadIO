1507b4e8a8df9e9c820c24fb095b52521412db13
Daniel Bogucki <daniel.bogucki95@gmail.com>
2019-05-06 21:01:30 +0200

    saving-to-file save/savetofile added

--- src/main/java/edu/bfpkg/controllers/FXMLNotepadController.java
+++ src/main/java/edu/bfpkg/controllers/FXMLNotepadController.java
@@ -3,16 +3,22 @@ package edu.bfpkg.controllers;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.*;
 import java.util.Scanner;
 
 public class FXMLNotepadController {
+
+    @FXML
+    private BorderPane borderPane;
 
     @FXML
     private MenuItem btnFind;
@@ -35,7 +41,10 @@ public class FXMLNotepadController {
     @FXML
     private TextArea textArea;
 
+    Stage stage;
+
     private File file;
+
 
     @FXML
     void close(ActionEvent event) {
@@ -59,17 +68,18 @@ public class FXMLNotepadController {
 
     @FXML
     void open(ActionEvent event) {
+        if (stage == null) setStage();
         FileChooser fileChooser = new FileChooser();
-
         //Set extension filter for text files
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("pliki TXT (*.txt)", "*.txt");
         fileChooser.getExtensionFilters().add(extFilter);
 
         //Show save file dialog
-        file = fileChooser.showOpenDialog(null);
+        file = fileChooser.showOpenDialog(stage);
 
         if (file != null) {
             textArea.setText(readFile());
+            stage.setTitle("Zaawansowany notatnik" + " (" + file.getName() + ")");
         }
     }
 
@@ -80,23 +90,37 @@ public class FXMLNotepadController {
 
     @FXML
     void save(ActionEvent event) {
+        if (stage == null) setStage();
+        if (file == null) {
+            saveToFile(event);
+        } else {
+            saveTextToFile(textArea.getText());
+            stage.setTitle("Zaawansowany notatnik" + " (" + file.getName() + ")");
+        }
+
+    }
+
+    @FXML
+    void saveToFile(ActionEvent event) {
+        if (stage == null) setStage();
         FileChooser fileChooser = new FileChooser();
-
-        //Set extension filter for text files
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("pliki TXT (*.txt)", "*.txt");
         fileChooser.getExtensionFilters().add(extFilter);
-
-        //Show save file dialog
-        file = fileChooser.showSaveDialog(null);
+        file = fileChooser.showSaveDialog(stage);
 
         if (file != null) {
             saveTextToFile(textArea.getText());
+            stage.setTitle("Zaawansowany notatnik" + " (" + file.getName() + ")");
         }
     }
 
     @FXML
     void undo(ActionEvent event) {
 
+    }
+
+    private void setStage() {
+        stage = (Stage) borderPane.getScene().getWindow();
     }
 
     private void saveTextToFile(String text) {
@@ -108,13 +132,13 @@ public class FXMLNotepadController {
 
             Alert confirm = new Alert(Alert.AlertType.INFORMATION);
             confirm.setTitle("Saving OK");
-            confirm.setHeaderText("File " + file.getName() + " properly saved");
+            confirm.setHeaderText("Plik " + file.getName() + " poprawnie zapisany");
             confirm.show();
 
         } catch (IOException ex) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Saving error");
-            alert.setHeaderText("There was an error during saving to file");
+            alert.setHeaderText("Błąd podczas zapisu do pliku");
             alert.setContentText(file.getAbsolutePath());
             alert.show();
         }
@@ -130,7 +154,7 @@ public class FXMLNotepadController {
         } catch (FileNotFoundException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Reading error");
-            alert.setHeaderText("There was an error during reading from file");
+            alert.setHeaderText("Błąd odczytu pliku");
             alert.setContentText(file.getAbsolutePath());
             alert.show();
         }
